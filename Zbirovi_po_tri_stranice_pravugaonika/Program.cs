ee6bb5f6081aeea81c8b63e0fce12573856e0b22
usern4mehere9 <91462628+usern4mehere9@users.noreply.github.com>
2021-09-29 09:23:57 +0200

    Add files via upload

---
+++ Zbirovi_po_tri_stranice_pravugaonika/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Zbirovi_po_tri_stranice_pravugaonika
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
