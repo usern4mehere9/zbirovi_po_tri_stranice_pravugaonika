f9a0f645c53696058fd216f309983d8391631d04
usern4mehere9 <91462628+usern4mehere9@users.noreply.github.com>
2021-09-30 10:24:01 +0200

    Add files via upload

---
+++ Zbirovi_po_tri_stranice_pravugaonika/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Zbirovi_po_tri_stranice_pravugaonika
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a, b, m, n, O, P;
+
+            m = double.Parse(Console.ReadLine());
+            n = double.Parse(Console.ReadLine());
+
+            b = ((2 * m) - n) / 3;
+            a = m - (2 * b);
+
+            O = (2 * a) + (2 * b);
+            P = a * b;
+
+            Console.WriteLine(Math.Round(O, 2));
+            Console.WriteLine(Math.Round(P, 2));
+        }
+    }
+}
